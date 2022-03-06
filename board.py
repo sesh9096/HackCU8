2502c6b7027409bec900b40f08e3f714ef45f20b
IzakKL <101017669+IzakKL@users.noreply.github.com>
2022-03-05 19:40:53 -0700

    Add files via upload

--- board.py
+++ board.py
@@ -5,13 +5,13 @@ pygame.init()
 
 #backgrounds= {(1,1):(0,0,0), (1,2):'map12.jpg', (1,3):(67, 190, 11), (2,1):(), (2,2):'map22.jpg', (2,3):(232, 199, 181), (3,1):(193, 158, 14), (3,2):(229, 97, 89), (3,3):(203, 175, 165)}
 
-backgrounds={1:'map12.jpg',2:'map22.jpg',3:'map22.jpg',4:'map22.jpg',5:'map22.jpg',6:'map22.jpg',7:'map22.jpg',8:'map22.jpg',9:'map22.jpg'}
+backgrounds={1:'map12.jpg',2:'map22.jpg',3:'map12.jpg',4:'map22.jpg',5:'map12.jpg',6:'map22.jpg',7:'map12.jpg',8:'map22.jpg',9:'map12.jpg'}
 
 class screen():
-    def __init__(self, width=800, height=800, location = 1):#(2,2)):
-        #self.title = backgrounds[location][1] #if we want to ad titles to each room
-        self.width = 800
-        self.height = 800
+    def __init__(self, title, width=800, height=800, location = 1):#(2,2)):
+        self.title = title #backgrounds[location][1] #if we want to ad titles to each room
+        self.width = width
+        self.height = height
         self.location = location
         bg = pygame.image.load(backgrounds[location])
         self.background = pygame.image.load(backgrounds[location])
@@ -20,9 +20,9 @@ class screen():
     def update_background(self, door_entered):
         current_location = self.location
         new_location = 1
-        if door_entered == "L":
+        if door_entered == "L" and current_location!=1:
             self.location-=1
-        else:
+        elif door_entered == 'R' and current_location!=9:
             self.location+=1
         #self.location = new_location
         bg = pygame.image.load(backgrounds[new_location])
