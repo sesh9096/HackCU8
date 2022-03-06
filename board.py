9ea940115498a8157994b11f459863224d28eefd
IzakKL <101017669+IzakKL@users.noreply.github.com>
2022-03-05 18:41:21 -0700

    Add files via upload
    The new version of the board. The code is not very good. Also, there is a test code at the bottom that needs to be put in a main.

--- board.py
+++ board.py
@@ -3,39 +3,48 @@ import pygame
 
 pygame.init()
 
-backgrounds= {(1,1):(0,0,0), (1,2):(110, 77, 209), (1,3):(67, 190, 11), (2,1):'map12.jpg', (2,2):'map22.jpg', (2,3):(232, 199, 181), (1,1):(193, 158, 14), (1,2):(229, 97, 89), (1,3):(203, 175, 165)}
+backgrounds= {(1,1):(0,0,0), (1,2):'map12.jpg', (1,3):(67, 190, 11), (2,1):(), (2,2):'map22.jpg', (2,3):(232, 199, 181), (3,1):(193, 158, 14), (3,2):(229, 97, 89), (3,3):(203, 175, 165)}
 
 class screen():
-    def __init__(self, title, width=800, height=800, location = (2,2)):
-        self
+    def __init__(self, width=800, height=800, location = (2,2)):
+        #self.title = backgrounds[location][1]
         self.width = 800
         self.height = 800
         self.location = location
+        bg = pygame.image.load(backgrounds[location])
         self.background = pygame.image.load(backgrounds[location])
-        
-    def display_screen(self):
-        #bg = pygame.image.load("bg.png")
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.screen.blit(self.background, (0, 0))
         
-    def update_screen(self, door_entered):
+    def update_background(selfdoor_entered):
         current_location = self.location
         new_location = ()
         if(door_entered == 'N'):
-            new_location = (current_location[0]-1,current_location[1]) 
+            new_location = (current_location[0]-1, current_location[1]) 
         elif(door_entered == 'S'):
-            new_location = (current_location[0]+1,current_location[1])
+            new_location = (current_location[0]+1, current_location[1])
         elif(door_entered == 'E'):
-            new_location = (current_location[0],current_location[1]-1)
-        else:
-            new_location = (current_location[0]-1,current_location[1]+1)
+            new_location = (current_location[0], current_location[1]-1)
+        elif door_entered == 'W':
+            new_location = (current_location[0]-1, current_location[1]+1)
         self.location = new_location
-        self.background = pygame.image.load(backgrounds[new_location])
-
-#screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+        bg = pygame.image.load(backgrounds[new_location])
+        self.background = pygame.transform.scale(bg, (self.width, self.height))
+        
+    #def update_doors(self):
+        #if 
+        
 scr = screen("start")
-scr.display_screen()
-scr.update_screen('N')
-scr.display_screen()
+#pygame.display.set_mode((self.width, self.height))
+run = True
+while(run):
+    scr.screen.blit(scr.background,(0,0))
+    #direction = ''
+    #for event in pygame.event.get():
+        #if event.type == SPACE:
+            #get direction
+            #scr.update_screen('N')
+    pygame.display.update()
+    #scr.update_background(direction)
+    #scr.update_doors()
+#scr.display_screen()
                                             
